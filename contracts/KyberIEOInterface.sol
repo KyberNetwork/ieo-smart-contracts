146c9c98e39bcc60b37df38800e5e89830230ff4
ilanDoron <ilan.on.the.road@gmail.com>
2018-05-02 12:56:16 +0300

    add user ID to IEO procedure

--- contracts/KyberIEOInterface.sol
+++ contracts/KyberIEOInterface.sol
@@ -2,8 +2,8 @@ pragma solidity ^0.4.23;
 
 
 interface KyberIEOInterface {
-    function contribute(address contributor, uint8 v, bytes32 r, bytes32 s) external payable returns(bool);
-    function getContributorRemainingCap(address contributor) external view returns(uint capWei);
+    function contribute(address contributor, uint userId, uint8 v, bytes32 r, bytes32 s) external payable returns(bool);
+    function getContributorRemainingCap(uint userId) external view returns(uint capWei);
     function getIEOId() external view returns(uint);
 }
 
